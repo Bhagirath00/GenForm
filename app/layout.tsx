915f2b111ed67b333db633ade51962254d419372
Aman Singh <amansingh080704@gmail.com>
2025-11-18 09:56:09 +0530

    last minute update also updation for the deoplyment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,14 @@
 export const dynamic = "force-dynamic";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/theme-provider";
 import {Toaster} from "react-hot-toast"
 import { Analytics } from '@vercel/analytics/next';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -31,7 +26,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${inter.variable} antialiased`}
         >
           <ThemeProvider
             attribute="class"
